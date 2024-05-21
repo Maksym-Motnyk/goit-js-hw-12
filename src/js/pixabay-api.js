ab340e72c5939d4469c9dc1d036e4e5677fd9167
Maksym Motnyk <maksummotnuk@gmail.com>
2024-05-21 13:27:48 +0300

    homework-js-12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,22 @@
+function showLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'block';
+}
+import axios from 'axios';
+
+axios.default.baseURL = BASE_URL;
+
+export function searchPhotos(input) {
+  showLoader();
+
+  const searchParams = new URLSearchParams({
+    key: '43803497-a801e9cfe7ea9bdd19d306bb3',
+    q: input.value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+
+  return fetch(url);
+}
---
+++ src/main.js
@@ -0,0 +1,56 @@
+import { searchPhotos } from './js/pixabay-api';
+import { markupInterface } from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { listImg } from './js/render-functions';
+
+const searchButton = document.querySelector('.searchButton');
+const clearInput = () => {
+  const input = document.querySelector('.input');
+  input.value = '';
+};
+
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+}
+
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  const input = document.querySelector('.input');
+
+  if (input.value.trim() == '') {
+    iziToast.error({
+      title: 'Error',
+      position: 'topRight',
+      message:
+        'The search field cannot be empty! Please enter the search query!',
+    });
+    return;
+  } else {
+    searchPhotos(input)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        hideLoader();
+        markupInterface(data);
+        if (!listImg.childElementCount) {
+          iziToast.error({
+            title: 'Error',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+        }
+      })
+      .catch(error => {
+        hideLoader();
+        console.error('Error:', error);
+      });
+  }
+  clearInput();
+});
